52199ca66c8de656be148b7fb51ec5c571136dac
montazeri_a <montazeri_a@office.zharfpouyan.net>
2022-06-28 10:13:57 +0430

    .

---
+++ packages/footman/acl/src/models/Role.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Footman\Acl\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Role extends Model
+{
+    use HasFactory;
+}
